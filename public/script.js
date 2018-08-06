106cc24c567b4736d7a6816949bc5e6f1804dd8e
Antonio Serna <antonelo@gmail.com>
2018-08-06 16:56:17 +0200

    Lecture 34 Adding vue resurce to project

--- public/script.js
+++ public/script.js
@@ -5,23 +5,18 @@ new Vue({
     data: {
         total: 0,
         items: [
-            { id: 1, title: 'Item 1' },
-            { id: 2, title: 'Item 2' },
-            { id: 3, title: 'Item 3' }
+            {id: 1, title: 'a1' },
+            {id: 2, title: 'a2' }
         ],
-        cart: [],
+        cart:  [],
         search: ''
     },
     methods: {
         onSubmit: function() {
-            this.$http
-                .get('/search/'.concat(this.search))
-                .then(function(res) {
-                    this.items = res.data;
-                });
+            console.log(this.$http);
         },
         addItem: function(index) {
-            this.total += 9.99;
+            this.total += PRICE;
             var item = this.items[index];
             var found = false;
             for (var i = 0; i < this.cart.length; i++) {
